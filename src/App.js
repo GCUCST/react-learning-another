8322c69a536267d239f17d8c1beadeb5644fe744
陈少桐 <1528841402@qq.com>
2020-06-30 11:30:40 +0800

    第一次提交

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+let time = new Date().toDateString()
+function App() {
+  return (
+    <div className="App">
+       {time}
+    </div>
+  );
+}
+
+export default App;
